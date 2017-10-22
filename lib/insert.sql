2f67117d1c0b1e26ff20730ead1715fefeca5af8
Dominic Chapman <dominicchapman@me.com>
2017-10-22 20:43:26 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES ("Mr. Chocolate", 32, "M", "brown", "aggressive", 1);
+INSERT INTO bears VALUES ("Rowdy", 16, "M", "white", "aggressive", 1);
+INSERT INTO bears VALUES ("Tabitha", 21, "F", "black", "peaceful", 0);
+INSERT INTO bears VALUES ("Sergeant Brown", 28, "M", "brown", "moody", 1);
+INSERT INTO bears VALUES ("Melissa", 14, "F", "pink", "grumpy", 1);
+INSERT INTO bears VALUES ("Grinch", 35, "F", "blue", "kind", 1);
+INSERT INTO bears VALUES ("Wendy", 35, "F", "jet black", "smart", 0);
+INSERT INTO bears VALUES (null, 32, "M", "brown", "generous", 1);
